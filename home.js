0f60d6cc519086dd58d97826b889d95a239aba87
vamsiyerragunta <makechinnasmile@gmail.com>
2020-11-07 08:15:02 +0530

    init

---
+++ home.js
@@ -0,0 +1,106 @@
+console.log('hello');
+
+// alert("vamsi");
+
+//How to werite comment inline
+
+var b = "shiny";
+console.log(b);
+
+var someNumber = 45;
+// console.log(someNumber);
+
+
+// var age = prompt("what is your age?");
+
+
+// document.getElementById("sometext").innerHTML = age;
+
+//Numbers in Javascript
+
+var num1 = 2.7;
+num1++;
+num1++;
+console.log(num1);
+
+//function
+
+function fun() {
+    alert("this is a  function");
+}
+fun();
+
+/* let's create funtion that takes name
+and says hello followed by name
+
+for example
+name: "vamsi"
+returm: "Hello Vamsi"
+
+*/
+function gretting(){
+    var name = prompt("what is your name?");
+    var result = 'Hello ' + name;
+    console.log(result);
+
+}
+gretting();
+
+//How do arguments work in function?
+
+//How do we add two numbers in function
+
+function addingnumbers(num1, num2){
+    var result = num1 + num2;
+    console.log(result);
+}
+addingnumbers("10", "10");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
